67513f7ad8f7b1d2c031dad45748057989f4b0fc
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2024-02-14 23:14:07 -0300

    cambios skills

--- app/ui/views/Skills.jsx
+++ app/ui/views/Skills.jsx
@@ -7,14 +7,20 @@ const Skills = () => {
   const [openProjects, setOpenProjects] = useState(false)
   const [projectName, setProjectName] = useState('')
 
+  const handleOnBlur = () => {
+    setTimeout(() => {
+      setOpenProjects(false)
+    }, 100)
+  }
+
   const technology = (name, icon, projects) => (
-    <div className='relative' onfo >
+    <div className='relative' onfo>
       {openProjects && projectName === name && (
-        <ul className='text-center w-[10rem] py-[.5rem] absolute bottom-[5rem] left-[-3rem] bg-secondary rounded-3xl ' >
+        <ul className='text-center w-[10rem] py-[.5rem] absolute bottom-[5rem] left-[-3rem] bg-secondary rounded-3xl '>
           <h3 className='font-bold text-lg'>{name}</h3>
           {projects?.map(({ name, link }) => (
             <li className='my-2 hover:font-bold hover:text-black transition-all duration-300'>
-              <a href={link} target={link === '/#home' ? '_top' : '_blank'} >
+              <a href={link} target={link === '/#home' ? '_top' : '_blank'}>
                 {name}
               </a>
             </li>
@@ -27,15 +33,10 @@ const Skills = () => {
           setOpenProjects(!openProjects)
           setProjectName(name)
         }}
-        onBlur={() => {
-          setTimeout(() => {
-            setOpenProjects(false)
-          }, 100 )
-        }}
+        onBlur={handleOnBlur}
         key={name}
         className='hover:scale-125 hover:text-secondary transition duration-300'
       >
-        
         {icon}
       </button>
     </div>
@@ -45,9 +46,7 @@ const Skills = () => {
     <section id='skills' className='mt-[4rem]'>
       <h2 className=' text-5xl mb-[3rem]'>Tech Skills</h2>
 
-      
-
-      <div className='h-auto mb-[3rem]  flex justify-evenly' >
+      <div className='h-auto mb-[3rem]  flex justify-evenly'>
         {technologies
           .slice(0, 8)
           .map(({ name, icon, projects }) => technology(name, icon, projects))}
